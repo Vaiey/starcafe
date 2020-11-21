566b2330f563470881d670998c2e689830497983
Vaiey <72122700+Vaiey@users.noreply.github.com>
2020-11-21 10:53:06 +0800

    HTML ENDTASK: Fast Food
    Star Cafe
    
    Take a break and have a cup of coffee

---
+++ order.js
@@ -0,0 +1,283 @@
+var P1 = 0;
+var Q1 = 0;
+
+function hc1(){
+  document.getElementById("pc").src="https://image.freepik.com/free-photo/cup-hot-americano-coffee-hot-espresso-coffee-wooden-table_41386-402.jpg";
+  
+  Q1 = Q1 + 1;
+  document.getElementById("prd").innerHTML = Q1 + " Caffé Americano";
+  
+  P1 = P1 + 110;
+  document.getElementById("prc").innerHTML = "₱" + P1;
+}
+
+var P2 = 0;
+var Q2 = 0;
+
+function cc1(){
+  document.getElementById("pc").src="https://image.kpopmap.com/2020/07/78c6057ca0e140e2a8de4a5e3d37f46b.jpg";
+  
+  Q2 = Q2 + 1;
+  document.getElementById("prd").innerHTML = Q2 + " Iced Caffé Americano";
+  
+  P2 = P2 + 115;
+  document.getElementById("prc").innerHTML = "₱" + P2;
+}
+
+var P3 = 0;
+var Q3 = 0;
+
+function hc2(){
+  document.getElementById("pc").src="https://vaya.in/recipes/wp-content/uploads/2019/02/Cappuccino.jpg";
+  
+  Q3 = Q3 + 1;
+  document.getElementById("prd").innerHTML = Q3 + " Cappuccino";
+  
+  P3 = P3 + 120;
+  document.getElementById("prc").innerHTML = "₱" + P3;
+}
+
+var P4 = 0;
+var Q4 = 0;
+
+function cc2(){
+  document.getElementById("pc").src="https://www.recipegirl.com/wp-content/uploads/2011/07/Iced-Coffee-11.jpg";
+  
+  Q4 = Q4 + 1;
+  document.getElementById("prd").innerHTML = Q4 + " Iced Coffee";
+  
+  P4 = P4 + 110;
+  document.getElementById("prc").innerHTML = "₱" + P4;
+}
+
+var P5 = 0;
+var Q5 = 0;
+
+function hc3(){
+  document.getElementById("pc").src="https://i2.wp.com/coffeebros.com/wp-content/uploads/2019/04/Espresso-crema-Here%E2%80%99s-why-it%E2%80%99s-important.png?fit=939%2C625&ssl=1";
+  
+  Q5 = Q5 + 1;
+  document.getElementById("prd").innerHTML = Q5 + " Espresso";
+  
+  P5 = P5 + 100;
+  document.getElementById("prc").innerHTML = "₱" + P5;
+}
+
+var P6 = 0;
+var Q6 = 0;
+
+function cc3(){
+  document.getElementById("pc").src="https://www.mercurynews.com/wp-content/uploads/2019/06/SJM-L-COLDCOFFEE-0628-03.jpg?w=862";
+  
+  Q6 = Q6 + 1;
+  document.getElementById("prd").innerHTML = Q6 + " Iced Espresso";
+  
+  P6 = P6 + 105;
+  document.getElementById("prc").innerHTML = "₱" + P6;
+}
+
+var P7 = 0;
+var Q7 = 0;
+
+function hc4(){
+  document.getElementById("pc").src="https://www.nespresso.com/ncp/res/resized/w400_h300/uploads/recipes/nespresso-recipes-Cafe-Latte-Do-Brazil.jpg";
+  
+  Q7 = Q7 + 1;
+  document.getElementById("prd").innerHTML = Q7 + " Caffé Latte";
+  
+  P7 = P7 + 120;
+  document.getElementById("prc").innerHTML = "₱" + P7;
+}
+
+var P8 = 0;
+var Q8 = 0;
+
+function cc4(){
+  document.getElementById("pc").src="https://i.pinimg.com/originals/98/b2/c9/98b2c9c87d7b6b1329a39bcf91f55219.jpg";
+  
+  Q8 = Q8 + 1;
+  document.getElementById("prd").innerHTML = Q8 + " Iced Caffé Latte";
+  
+  P8 = P8 + 120;
+  document.getElementById("prc").innerHTML = "₱" + P8;
+}
+
+var P9 = 0;
+var Q9 = 0;
+
+function hc5(){
+  document.getElementById("pc").src="https://www.nespresso.com/ncp/res/uploads/recipes/nespresso-recipes-Espresso-Macchiato-by-Nespresso.jpg";
+  
+  Q9 = Q9 + 1;
+  document.getElementById("prd").innerHTML = Q9 + " Espresso Macchiato";
+  
+  P9 = P9 + 105;
+  document.getElementById("prc").innerHTML = "₱" + P9;
+}
+
+var P10 = 0;
+var Q10 = 0;
+
+function cc5(){
+  document.getElementById("pc").src="https://www.readyseteat.com/sites/g/files/qyyrlu501/files/uploadedImages/img_9611_25311.jpg";
+  
+  Q10 = Q10 + 1;
+  document.getElementById("prd").innerHTML = Q10 + " Iced Caramel Macchiato";
+  
+  P10 = P10 + 115;
+  document.getElementById("prc").innerHTML = "₱" + P10;
+}
+
+var P11 = 0;
+var Q11 = 0;
+
+function bf1(){
+  document.getElementById("pc").src="https://www.thespruceeats.com/thmb/LYPl7Inm-dn62nPFsRVCkTSaljc=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/chipotle-bacon-burrito-58ada45c3df78c345b9f0cf8.jpg";
+  
+  Q11 = Q11 + 1;
+  document.getElementById("prd").innerHTML = Q11 + " Bacon, Egg & Cheese Burrito";
+  
+  P11 = P11 + 280;
+  document.getElementById("prc").innerHTML = "₱" + P11;
+}
+
+var P12 = 0;
+var Q12 = 0;
+
+function bd1(){
+  document.getElementById("pc").src="https://www.seriouseats.com/recipes/images/2017/01/20170127-bagels-vicky-wasik-2-1500x1125.jpg";
+  
+  Q12 = Q12 + 1;
+  document.getElementById("prd").innerHTML = Q12 + " Classic Bagel";
+  
+  P12 = P12 + 20;
+  document.getElementById("prc").innerHTML = "₱" + P12;
+}
+
+var P13 = 0;
+var Q13 = 0;
+
+function bf2(){
+  document.getElementById("pc").src="https://irepo.primecp.com/2015/06/223330/Bacon-and-Egg-Breakfast-Grilled-Cheese_ExtraLarge700_ID-1029586.jpg?v=1029586";
+  
+  Q13 = Q13 + 1;
+  document.getElementById("prd").innerHTML = Q13 + " Bacon & Egg Grilled Cheese Sandwich";
+  
+  P13 = P13 + 220;
+  document.getElementById("prc").innerHTML = "₱" + P13;
+}
+
+var P14 = 0;
+var Q14 = 0;
+
+function bd2(){
+  document.getElementById("pc").src="https://www.panasonic.com/content/dam/Panasonic/Global/recipe/2019/cinnamon-roll/pub-cinnamon-roll_main_thumbnail_square.jpg";
+  
+  Q14 = Q14 + 1;
+  document.getElementById("prd").innerHTML = Q14 + " Cinnamon Rolls";
+  
+  P14 = P14 + 40;
+  document.getElementById("prc").innerHTML = "₱" + P14;
+}
+
+var P15 = 0;
+var Q15 = 0;
+
+function bf3(){
+  document.getElementById("pc").src="https://natashaskitchen.com/wp-content/uploads/2020/06/Chicken-Sandwich-4-728x1092.jpg";
+  
+  Q15 = Q15 + 1;
+  document.getElementById("prd").innerHTML = Q15 + " Crispy Chicken Sandwich";
+  
+  P15 = P15 + 120;
+  document.getElementById("prc").innerHTML = "₱" + P15;
+}
+
+var P16 = 0;
+var Q16 = 0;
+
+function bd3(){
+  document.getElementById("pc").src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2014/7/17/1/FN_Simple-Chocolate-Chip-Cookies_s4x3.jpg.rend.hgtvcom.826.620.suffix/1438794106265.jpeg";
+  
+  Q16 = Q16 + 1;
+  document.getElementById("prd").innerHTML = Q16 + " Chocolate Chip Cookie";
+  
+  P16 = P16 + 10;
+  document.getElementById("prc").innerHTML = "₱" + P16;
+}
+
+var P17 = 0;
+var Q17 = 0;
+
+function bd4(){
+  document.getElementById("pc").src="https://data.thefeedfeed.com/static/2020/01/03/15780666485e0f62d85f8e6.jpg";
+  
+  Q17 = Q17 + 1;
+  document.getElementById("prd").innerHTML = Q17 + " Fudgy Dark Chocolate Brownies";
+  
+  P17 = P17 + 15;
+  document.getElementById("prc").innerHTML = "₱" + P17;
+}
+
+var P18 = 0;
+var Q18 = 0;
+
+function bd5(){
+  document.getElementById("pc").src="https://www.theflavorbender.com/wp-content/uploads/2020/05/French-Croissants-SM-2363.jpg";
+  
+  Q18 = Q18 + 1;
+  document.getElementById("prd").innerHTML = Q18 + " French Croissant";
+  
+  P18 = P18 + 30;
+  document.getElementById("prc").innerHTML = "₱" + P18;
+}
+
+function undo(){
+  alert("Are you sure you want to undo your order?");
+  document.getElementById("pc").src="https://i.pinimg.com/564x/78/cb/ac/78cbac370d8237aaca30474402aeeea8.jpg";
+  
+  Q1 = 0;
+  P1 = 0;
+  Q2 = 0;
+  P2 = 0;
+  Q3 = 0;
+  P3 = 0;
+  Q4 = 0;
+  P4 = 0;
+  Q5 = 0;
+  P5 = 0;
+  Q6 = 0;
+  P6 = 0;
+  Q7 = 0;
+  P7 = 0;
+  Q8 = 0;
+  P8 = 0;
+  Q9 = 0;
+  P9 = 0;
+  Q10 = 0;
+  P10 = 0;
+  Q11 = 0;
+  P11 = 0;
+  Q12 = 0;
+  P12 = 0;
+  Q13 = 0;
+  P13 = 0;
+  Q14 = 0;
+  P14 = 0;
+  Q15 = 0;
+  P15 = 0;
+  Q16 = 0;
+  P16 = 0;
+  Q17 = 0;
+  P17 = 0;
+  Q18 = 0;
+  P18 = 0;
+  
+  document.getElementById("prd").innerHTML = "";
+  document.getElementById("prc").innerHTML = "";
+}
+
+function complete(){
+  alert("Order Completed.");
+  alert("Please proceed with the payment.");
+}
